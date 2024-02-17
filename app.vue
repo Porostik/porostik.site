d6a7f6256d23070d08dc9b857712a2fc872d44fd
Porostik <porostik2@yandex.ru>
2024-02-17 12:50:49 +0600

    feat: styles

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="main">
+    <NuxtPage />
   </div>
 </template>
+
+<styles scoped lang="scss">
+.main {
+  background: $primary;
+  width: 100%;
+  height: 100%;
+}
+</styles>
